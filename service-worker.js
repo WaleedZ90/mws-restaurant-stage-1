286307374c62c745ecc1942b537d6de6b0233f91
Waleed Tayseer <waleedtayseer@hotmail.com>
2018-03-31 21:24:05 +0200

    FInished the service worker caching

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,62 @@
 const headers = {
     'content-type': 'text/html'
-}
+};
 
-self.addEventListener('fetch', function (event) {
-    // event.respondWith(
-    //     new Response('Hello <strong>Lola!</strong>', {
-    //         headers: {
-    //             'Content-Type': 'text/html'
-    //         }
-    //     })
-    // );
+const FILES_TO_CACHE = [
+    '/',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/restaurant.html',
+    '/js/restaurant_info.js',
+    '/js/dbhelper.js',
+    '/js/main.js',
 
-    // Checking for images
-    // if (event.request.url.endsWith('.jpg')) {
-    //     event.respondWith(
-    //         fetch('img/10.jpg')
-    //     );
-    // }
+];
 
-    event.respondWith(
-        fetch(event.request).then((response) => {
-            if (response.status == 404) {
-                return new Response("<h1>No resources were found.</h1>", { headers })
-            }
-            return response;
-        }).catch((error) => {
-            return new Response("<h1>Something went wrong!</h1>", { headers })
+const staticCacheName = 'restaurant-static-v2';
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.addAll(FILES_TO_CACHE)
+        })
+        .then(() => {
+            console.log('Content cached!')
+        })
+        .catch(() => {
+            console.log('Content not cached!')
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('restaurnt-') && cacheName !== staticCacheName;
+                }).map(function (cacheName) {
+                    return cache.delete(cacheName);
+                })
+            );
         })
     );
 })
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
