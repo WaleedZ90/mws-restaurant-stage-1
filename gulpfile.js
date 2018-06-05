092657fdd0b0989a080ff28ec2859559b97218ba
Waleed Tayseer <Waleed.Tayseer@itworx.com>
2018-06-05 14:24:38 +0200

    Fixing gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
-gulp.task('copy', (done) => {
-    return gulp.src('../index.html')
-        .pipe(gulp.dest('dist'));
+// gulp version should be
+// CLI version 3.9.1
+// Local version 3.9.1
+
+gulp.task('copy-html', function (done) {
+    gulp.src('./*.html')
+        .pipe(gulp.dest('./dist'));
+    done();
 })
+
 
 gulp.task('scripts', function (done) {
     gulp.src('js/**/*.js')
